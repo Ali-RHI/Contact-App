2fe6263b5a561ef37aedc33167081332b1d69092
Ali Rouhi <alirhtg@gmail.com>
2024-04-14 18:40:12 +0330

    contactslist structure

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -4,11 +4,17 @@ function ContactsList({ contacts }) {
     return (
         <div>
             <h3>Contacts List</h3>
-            <ul>
+            { contacts.length ? (<ul>
                 { contacts.map((contact) => (
-                    <li key={ contact.id }>{ contact.firstName }</li>
+                    <li key={ contact.id }>
+                        <p>{ contact.firstName } { contact.lastName }</p>
+                        <p><span>📥</span> { contact.email }</p>
+                        <p><span>📞</span> { contact.phone }</p>
+                        <button>🗑️</button>
+                    </li>
                 )) }
-            </ul>
+            </ul>) : <p>No contacts yet!</p> }
+
         </div>
     )
 }
